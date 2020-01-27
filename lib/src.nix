ba497d7fb589d7f900bf64d795ebdb040e3ab532
Alexander Bantyev <balsoft@balsoft.ru>
2020-01-27 13:27:41 +0300

    Fix typo

--- lib/src.nix
+++ lib/src.nix
@@ -14,7 +14,7 @@
   # name: name of the directory.
   #       Yes it is boring to always specify the name explicitly,
   #       but this way we guarantee no unexpected issues with caching.
-  # src: direcotry to clean. This is typically `./.`.
+  # src: directory to clean. This is typically `./.`.
   cleanGit = name: src:
     haskell-nix.lib.cleanGit {
       src = haskell-nix.lib.cleanSourceWith {
