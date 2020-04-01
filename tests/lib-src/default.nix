8d5f9efd56e394e3dcdf4c1c3ee7989f6c380d7a
Kirill Elagin <kirelagin@gmail.com>
2020-04-01 14:59:52 +0300

    Move lib/src tests and add licence

--- tests/lib-src/default.nix
+++ tests/lib-src/default.nix
@@ -1,14 +1,18 @@
-let pkgs = import (import ../nix/sources.nix).nixpkgs { };
-in with import ../.;
+# SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+let pkgs = import (import ../../nix/sources.nix).nixpkgs { };
+in with import ../../.;
 with lib.src; {
   no-hash-mismatch = (toString (cleanGit "test" ./test-foo))
     == (toString (cleanGit "test" ./test-bar));
   cleans-git = let
-    commit = pkgs.lib.commitIdFromGitRepo ../.git;
+    commit = pkgs.lib.commitIdFromGitRepo ../../.git;
     githubSrc = builtins.fetchGit {
       url = "https://github.com/serokell/serokell.nix";
       rev = commit;
     };
   in (toString (cleanGit "serokell.nix" githubSrc))
-  == (toString (cleanGit "serokell.nix" ../.));
+  == (toString (cleanGit "serokell.nix" ../../.));
 }
