1e4469f2151990d23ef353cedfc9349df4c750b5
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2020-12-17 10:54:22 +0300

    [INT-158] Remove custom oauth2_proxy module
    It moved to pegasus-infra, because it's only used by serokell-website

--- overlay/default.nix
+++ overlay/default.nix
@@ -12,7 +12,6 @@ in
   # Uses sources.mix-to-nix and sources.gitignore
   nixUnstable = inputs.nix-unstable.defaultPackage.${final.system};
 
-  oauth2_proxy = callPackage ./oauth2_proxy { };
   youtrack = callPackage ./youtrack.nix { };
 
   build = {
