b94e265f07dc5e2bf49175aabcb8900535660e72
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2021-03-09 16:45:11 +0300

    [OPS-1174] Configure dhcp to wait for an ipv4 address on AWS instances

--- modules/virtualization/ec2.nix
+++ modules/virtualization/ec2.nix
@@ -6,4 +6,9 @@
 
   # amazon-init service fetches system config from userdata, we don't need it
   virtualisation.amazon-init.enable = false;
+
+  # By default 'network-online.target' is activated as soon as either an ipv4
+  # or an ipv6 address has been assigned. But for AWS instances we want to wait
+  # for an ipv4 address, because DNS resolving will not work without it
+  networking.dhcpcd.wait = lib.mkDefault "ipv4";
 }
