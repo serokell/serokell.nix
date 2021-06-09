0ce2e0c59cb698c25630d2140f896d70d664fe8b
notgne2 <gen2@gen2.space>
2021-06-08 18:27:43 -0700

    Harden nginx options (#29)
    * Enable recommendedGzipSettings, don't open nginx firewall with nginx disabled, and add recommended hardening configuration from https://nixos.wiki/wiki/Nginx
    
    * Remove overly strict proxy_cookie_path policy
    
    * Remove X-XSS-Protection header, make Referrer-Policy stricter (this is the default policy now on most browsers)

--- modules/common.nix
+++ modules/common.nix
@@ -61,9 +61,27 @@
     appendHttpConfig = ''
       if_modified_since off;
     '';
+    recommendedGzipSettings = lib.mkDefault true;
     recommendedOptimisation = lib.mkDefault true;
     recommendedProxySettings = lib.mkDefault true;
     recommendedTlsSettings = lib.mkDefault true;
+    commonHttpConfig = ''
+      # Add HSTS header with preloading to HTTPS requests.
+      # Adding this header to HTTP requests is discouraged
+      map $scheme $hsts_header {
+          https   "max-age=31536000; includeSubdomains; preload";
+      }
+      add_header Strict-Transport-Security $hsts_header;
+
+      # Minimize information leaked to other domains
+      add_header 'Referrer-Policy' 'strict-origin-when-cross-origin';
+
+      # Disable embedding as a frame
+      add_header X-Frame-Options DENY;
+
+      # Prevent injection of code in other mime types (XSS Attacks)
+      add_header X-Content-Type-Options nosniff;
+    '';
   };
 
   security.acme = {
