3076c70cf7c32117c74887cfb30b7108d5573bd3
Philipp Herzog <philipp.herzog@protonmail.com>
2023-01-20 12:15:46 +0100

    Introduce minimal serokell-users module for darwin

---
+++ modules/serokell-users-options.nix
@@ -0,0 +1,42 @@
+{lib, ...}:
+let
+  inherit (lib) mkOption types;
+in
+{
+  wheelUsers = mkOption {
+    type = types.listOf types.str;
+    description = "Users added to wheel group";
+    example = [ "gosha" "masha" ];
+    default = [
+      "gromak"
+      "jaga"
+      "sweater"
+      "balsoft"
+      "rvem"
+      "cab404"
+      "magicrb"
+      "maelstroem"
+    ];
+  };
+
+  wheelUsersExtraGroups = mkOption {
+    default = [ ];
+    type = types.listOf types.str;
+    description = "Extra groups added to users in wheelUsers";
+    example = [ "docker" ];
+  };
+
+  regularUsers = mkOption {
+    default = [ ];
+    type = types.listOf types.str;
+    description = "Regular users";
+    example = [ "misha" "vasya" "petya" ];
+  };
+
+  regularUsersExtraGroups = mkOption {
+    default = [ ];
+    type = types.listOf types.str;
+    description = "Extra groups added to all users";
+    example = [ "systemd-journal" ];
+  };
+}
